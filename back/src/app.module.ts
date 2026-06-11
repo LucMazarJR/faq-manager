fc505d70b548adf10d34cebed83f600cec231a26
LucianoMazar <lmazaraojr@gmail.com>
2026-06-11 14:25:17 -0300

    chore(back): configure moongose

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { FaqsModule } from './faqs/faqs.module';
 import { HealthResponse } from './dto/health.dto';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FaqsModule, HealthResponse, ConfigModule.forRoot()],
+  imports: [
+    FaqsModule,
+    HealthResponse,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL || ''),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
