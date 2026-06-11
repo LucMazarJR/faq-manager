8c313fb5b4d860b0e4c9c50aa2986f55007349d7
LucianoMazar <lmazaraojr@gmail.com>
2026-06-11 12:29:19 -0300

    feat(back): health check route

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FaqsModule } from './faqs/faqs.module';
+import { HealthResponse } from './dto/health.dto';
 
 @Module({
-  imports: [FaqsModule],
+  imports: [FaqsModule, HealthResponse],
   controllers: [AppController],
   providers: [AppService],
 })
