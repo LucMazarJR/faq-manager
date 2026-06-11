2c1088fa63bb5626981ab1e549a6b81a108fc893
LucianoMazar <lmazaraojr@gmail.com>
2026-06-11 14:55:24 -0300

    chore(back): configure mongoose schema

--- back/src/faqs/faqs.module.ts
+++ back/src/faqs/faqs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FaqsService } from './faqs.service';
 import { FaqsController } from './faqs.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FaqSchema } from './schemas/faq.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Faqs', schema: FaqSchema }])],
   controllers: [FaqsController],
   providers: [FaqsService],
 })
